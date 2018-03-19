036c1f88b5ecd98306f541bd2e772cbac9fab227
GamerWang <GamerWang@Outlook.com>
2018-03-19 21:11:01 +0800

    Learn bool variables and operations

---
+++ variableBool.py
@@ -0,0 +1,36 @@
+'''
+# basic operations
+# a > b
+# a < b
+# a >= b
+# a <= b
+# a == b
+# a != b
+# not a
+# a and b
+# a or b
+'''
+
+# operation and value tests
+a = 1 < 3
+print a
+b = 1
+c = 3
+print b > c
+print b == c
+print b != c
+print a != b
+print a == b
+b = 2
+print a == b
+print False == 0
+d = False + 1
+print d
+print not d
+print 1 and 2
+print 0 and 1
+print False or 1
+print 1 or False
+print False and 1
+print 1 and False
+print 1 or (1 + 1 and 3)

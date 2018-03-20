12df27585dfe2c4cd16b13c79b1c24819d16f36d
GamerWang <GamerWang@Outlook.com>
2018-03-20 18:16:11 +0800

    Learn while and randint

--- controller.py
+++ controller.py
@@ -1,4 +1,8 @@
+# import modules
+from random import randint # import method for random int
+
 # if controller test
+'''
 x = 0
 if x:
     print x
@@ -15,16 +19,29 @@ else:
     if True:
         print 1
     print "not zero"
-
+'''
+# while controller test
+'''
+counter = 0
+while counter < 10:
+    print counter
+    counter += 1
+    # counter++     # this is not valid
+    if(counter == 5):
+        break
+'''
 # guess number game
-'''
-num = 10
+
+num = randint(1, 100)
+correct = False
 print 'Guess what I think?'
-answer = int(input())
-if answer < num:
-    print 'Too small!'
-if answer > num:
-    print 'Too large!'
-if answer == num:
-    print 'BINGO!'
-'''
+
+while not correct:
+    answer = int(input())
+    if answer < num:
+        print 'Too small!'
+    if answer > num:
+        print 'Too large!'
+    if answer == num:
+        correct = True
+        print 'BINGO!'

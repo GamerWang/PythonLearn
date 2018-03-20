8f4a6ff64e89d5a8959ff3cf276ba096ec761b2d
GamerWang <GamerWang@Outlook.com>
2018-03-20 18:05:22 +0800

    Learn new aspects about input()

--- input.py
+++ input.py
@@ -1,6 +1,15 @@
-# input test
+'''
+# int, float and bool variable do not need '' or ""
+# string variable must have '' or ""
+# bool variable must be True or False, lowercase will not work
+'''
 
+# input test
 print "Who do you think I am?"
 you = input()
 print "Oh, yes! I am a"
 print you
+
+thisIsLove = input()
+if thisIsLove:
+    print "Stay with me"
